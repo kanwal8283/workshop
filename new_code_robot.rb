b9e65d7188773b3d3f5e9c330049e0c443fbe254
Kamal <kanwal.singh@gmail.com>
2013-04-15 17:40:30 +1000

    Update new_code_robot.rb

--- new_code_robot.rb
+++ new_code_robot.rb
@@ -1,4 +1,3 @@
-
   def command arg
     case arg
       when (what_cmd(arg) == 'PLACE')
@@ -11,4 +10,22 @@
         "Error, first command must be PLACE"
     end
 
+  end
+
+
+ def left
+    @placed_on_grid = true
+    @direction = @direction - 1
+    if (@direction < 0)
+      @direction = @direction + 4
+    end
+  end
+
+
+  def right
+    @placed_on_grid = true
+    @direction = @direction + 1
+    if (@direction > 3)
+      @direction = @direction - 4
+    end
   end
